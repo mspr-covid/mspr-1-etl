0856e5d283a576e035998e79f209dd75688f80b7
Nouster <djebali.mohamed88@gmail.com>
2025-06-01 08:19:18 +0200

    fix build

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import LoginPage from "./pages/LoginPage";
-import DataPage from "./pages/DataPage";
-import ManagementPage from "./pages/ManagementPage";
-import PredictionPage from "./pages/PredictionPage";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 const router = createBrowserRouter([
@@ -15,18 +12,6 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <LoginPage />,
-      },
-      {
-        path: '/data',
-        element: <ProtectedRoute><DataPage /></ProtectedRoute>,
-      },
-      {
-        path: '/manage',
-        element: <ProtectedRoute><ManagementPage /></ProtectedRoute>,
-      },
-      {
-        path: '/predict',
-        element: <ProtectedRoute><PredictionPage /></ProtectedRoute>,
       },
     ],
   },
