3dc1aaab27931338eb578fcee5e865e0a574fd38
Nouster <djebali.mohamed88@gmail.com>
2025-06-12 07:56:15 +0000

    Predict page with V2 model

---
+++ front/src/types/CountryInputPredict.tsx
@@ -0,0 +1,13 @@
+interface CountryInputPredict {
+	continent: string;
+	who_region: string;
+	country: string;
+	population: number;
+	total_recovered: number;
+	active_cases: number;
+	serious_critical: number;
+	total_tests: number;
+	new_total_cases: number;
+}
+
+export default CountryInputPredict;
