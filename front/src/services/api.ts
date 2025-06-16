27a32dd2a04fc6754f719a8e0c19879c1dd17a4f
Mouhamed <mouhamediop114@gmail.com>
2025-06-16 03:26:33 +0200

    Redirect user to login if token is invalid or expired with interceptor axios and tokenIsValid function

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -11,6 +11,18 @@ const api = axios.create({
 	},
 });
 
+api.interceptors.response.use(
+	(response) => response,
+	(error) => {
+		if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+			localStorage.removeItem("token");
+			window.location.href = "/login";
+		}
+		return Promise.reject(error);
+	}
+);
+
+
 // Add token to requests if available
 api.interceptors.request.use((config) => {
 	const token = localStorage.getItem("token");
